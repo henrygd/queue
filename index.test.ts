7563dcd4739442610d30aed3f897f3e29101ddc8
Henry Dollman <hank@henrygd.me>
2024-06-11 22:57:12 -0400

    update tests

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,118 @@
 import { test, expect } from 'bun:test'
-import { newQueue } from '.'
+import { newQueue } from './index.ts'
+// import { newQueue as slimQueue } from './slim/index.slim.ts'
 
-test('should process 2 promises concurrently', async () => {
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+// type CreateQueue = typeof newQueue | typeof slimQueue
+type CreateQueue = typeof newQueue
+
+// describe('main', () => {
+test('should process 2 promises concurrently', () => testConcurrency(newQueue))
+
+test('should handle promise rejections correctly', () => testRejections(newQueue))
+
+test('greater concurrency should be faster', async () => {
+	const loops = 10
+	const waitTime = 50
+	const runTimes = [] as number[]
+
+	for (let i = 3; i < 5; i++) {
+		let queue = newQueue(i)
+		let start = performance.now()
+		for (let i = 0; i < loops; i++) {
+			queue.add(() => wait(waitTime))
+		}
+		await queue.done()
+		runTimes.push(performance.now() - start)
+	}
+
+	const [timeOne, timeTwo] = runTimes
+	expect(timeTwo).toBeLessThan(timeOne)
+})
+
+test('size should return the number of promises in the queue', async () => {
 	const queue = newQueue(2)
+	expect(queue.size()).toBe(0)
+	for (let i = 0; i < 10; i++) {
+		queue.add(() => wait(10))
+	}
+	expect(queue.size()).toBe(10)
+	await wait(15)
+	expect(queue.size()).toBe(8)
+	await wait(90)
+	expect(queue.size()).toBe(0)
+})
+
+test('active should return the number of active jobs', async () => {
+	const queue = newQueue(4)
+	expect(queue.active()).toBe(0)
+	for (let i = 0; i < 10; i++) {
+		queue.add(() => wait(10))
+	}
+	expect(queue.active()).toBe(4)
+	await wait(25)
+	expect(queue.active()).toBe(2)
+	await wait(10)
+	expect(queue.active()).toBe(0)
+})
+
+test('done should work even if the queue is empty', async () => {
+	const queue = newQueue(2)
+	await queue.done()
+})
+
+test('clear should clear the queue', async () => {
+	const queue = newQueue(2)
+	const runTimes = [] as number[]
+
+	for (let i = 0; i < 2; i++) {
+		let start = performance.now()
+		for (let i = 0; i < 10; i++) {
+			queue.add(() => wait(50))
+		}
+		if (i === 1) {
+			setTimeout(queue.clear, 110)
+		}
+		await queue.done()
+		runTimes.push(performance.now() - start)
+	}
+
+	const [runOne, runTwo] = runTimes
+	expect(runOne).toBeGreaterThan(240)
+	expect(runTwo).toBeGreaterThan(140)
+	expect(runTwo).toBeLessThan(160)
+})
+// })
+
+// describe('slim', () => {
+// 	test('should process 2 promises concurrently', () => testConcurrency(slimQueue))
+
+// 	test('should handle promise rejections correctly', () => testRejections(slimQueue))
+
+// 	test('greater concurrency should be faster', async () => {
+// 		const loops = 10
+// 		const waitTime = 50
+// 		const runTimes = [] as number[]
+
+// 		for (let i = 3; i < 5; i++) {
+// 			let jobs = [] as Promise<unknown>[]
+// 			let queue = slimQueue(i)
+// 			let start = performance.now()
+// 			for (let i = 0; i < loops; i++) {
+// 				jobs.push(queue.add(() => wait(waitTime)))
+// 			}
+// 			await Promise.all(jobs)
+// 			runTimes.push(performance.now() - start)
+// 		}
+
+// 		const [timeOne, timeTwo] = runTimes
+// 		expect(timeTwo).toBeLessThan(timeOne)
+// 	})
+// })
+
+async function testConcurrency(createQueue: CreateQueue) {
+	const queue = createQueue(2)
 	const running: number[] = []
 	const results: number[] = []
 
@@ -57,10 +167,10 @@ test('should process 2 promises concurrently', async () => {
 	await Promise.all([p1, p2, p3, p4, ...checks])
 
 	expect(results).toEqual([2, 1, 3, 4])
-})
+}
 
-test('should handle promise rejections correctly', async () => {
-	const queue = newQueue(2)
+async function testRejections(createQueue: CreateQueue) {
+	const queue = createQueue(2)
 
 	const createPromise = (value: number, delay: number, shouldReject: boolean) => () =>
 		new Promise<number>((resolve, reject) => {
@@ -89,4 +199,4 @@ test('should handle promise rejections correctly', async () => {
 
 	expect(results).toEqual([1, 3])
 	expect(errors).toEqual(['Promise 2 failed', 'Promise 4 failed'])
-})
+}
--- playground.ts
+++ playground.ts
@@ -1,24 +1,25 @@
 import { newQueue } from './index.ts'
 
+const queue = newQueue(2)
+
+const pokemon = ['ditto', 'hitmonlee', 'pidgeot', 'poliwhirl', 'golem', 'charizard']
+
+for (const name of pokemon) {
+	queue.add(async () => {
+		const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+		const json = await res.json()
+		console.log(`${json.name}: ${json.height * 10}cm | ${json.weight / 10}kg`)
+	})
+}
+
+console.log('running')
+await queue.done()
+console.log('done')
+
 // const queue = newQueue(5)
 
-// const sites = [
-// 	'https://google.com',
-// 	'https://github.com',
-// 	'https://youtube.com',
-// 	'https://twitter.com',
-// 	'https://facebook.com',
-// 	'https://reddit.com',
-// ]
+// const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
-// for (const site of sites) {
-// 	queue.add(() => fetch(site)).then((res) => console.log(site, res.status))
+// for (let i = 1; i <= 50; i++) {
+// 	queue.add(() => wait(500)).then(() => console.log(i, 'done'))
 // }
-
-const queue = newQueue(5)
-
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-
-for (let i = 1; i <= 50; i++) {
-	queue.add(() => wait(500)).then(() => console.log(i, 'done'))
-}
